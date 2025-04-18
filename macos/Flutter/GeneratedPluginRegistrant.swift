a2fd614dd8fafb19972e7192c0b14c09dfb9db36
Pham Phu Loc <phamphuloc7663@gmail.com>
2025-04-18 10:09:00 +0700

    push

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,10 +10,7 @@ import flutter_secure_storage_macos
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-<<<<<<< HEAD
-=======
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
->>>>>>> 12bc26aa5e07226e69f58fc9884236cc85086f5a
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
